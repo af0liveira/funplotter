b565a11b096cbf4a4aeb7a6c99a824d2b0a8197d
af0liveira <af0liveira@icloud.com>
2024-05-23 17:28:20 +0200

    Refactor app.py
    Remove code duplication in `app.py`.

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ app = Flask(__name__)
 
 functions: list[dict]= []
 
+def parse_function(f_expr: str, x_min: float, x_max: float) -> dict:
+    x_values = np.linspace(x_min, x_max, num=101, endpoint=True)
+    y_values = eval(f_expr.replace('x', 'x_values'))
+
+    function = {
+        'label': f_expr,
+        'x_min': x_min,
+        'x_max': x_max,
+        'data': [{'x': x, 'y': y} for x, y in zip(x_values, y_values)],
+    }
+
+    return function
+
+
 @app.route('/')
 def index():
     return render_template('index.html', functions=functions)
@@ -15,18 +29,9 @@ def index():
 def add_function():
     function = request.form.get('addFunction')
     x_min = float(request.form.get('setXMin'))
-    x_max = float(request.form.get('setXMax'))
+    x_max = float(request.form.get('setXMax')) 
 
-    x_values = np.linspace(x_min, x_max, num=101, endpoint=True)
-    y_values = eval(function.replace('x', 'x_values'))
-
-    function_dict = {
-        'label': function,
-        'x_min': x_min,
-        'x_max': x_max,
-        'data': [{'x': x, 'y': y} for x, y in zip(x_values, y_values)],
-    }
-
+    function_dict = parse_function(function, x_min, x_max)
     functions.append(function_dict)
 
     return redirect('/')
@@ -42,16 +47,7 @@ def update_function(id: int):
     x_min = float(request.form.get(f'setXMin{id}'))
     x_max = float(request.form.get(f'setXMax{id}'))
 
-    x_values = np.linspace(x_min, x_max, num=101, endpoint=True)
-    y_values = eval(function.replace('x', 'x_values'))
-
-    function_dict = {
-        'label': function,
-        'x_min': x_min,
-        'x_max': x_max,
-        'data': [{'x': x, 'y': y} for x, y in zip(x_values, y_values)],
-    }
-
+    function_dict = parse_function(function, x_min, x_max)
     functions[id] = function_dict
 
     return redirect('/')
