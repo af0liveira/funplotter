19d11ee7427c2f4267b6bf7f66d89a9ed80fb69e
af0liveira <af0liveira@icloud.com>
2024-05-23 15:03:20 +0200

    Implement update/delete buttons for plotted functions
    The settings used to plot each function remain visible on the
    application, so the function can be modified or deleted by the user.

--- app.py
+++ app.py
@@ -22,10 +22,37 @@ def add_function():
 
     function_dict = {
         'label': function,
+        'x_min': x_min,
+        'x_max': x_max,
         'data': [{'x': x, 'y': y} for x, y in zip(x_values, y_values)],
     }
 
     functions.append(function_dict)
+
+    return redirect('/')
+
+@app.route('/delete_function/<int:id>', methods=['POST'])
+def delete_function(id: int):
+    del functions[id]
+    return redirect('/')
+
+@app.route('/update_function/<int:id>', methods=['POST'])
+def update_function(id: int):
+    function = request.form.get(f'function{id}')
+    x_min = float(request.form.get(f'setXMin{id}'))
+    x_max = float(request.form.get(f'setXMax{id}'))
+
+    x_values = np.linspace(x_min, x_max, num=101, endpoint=True)
+    y_values = eval(function.replace('x', 'x_values'))
+
+    function_dict = {
+        'label': function,
+        'x_min': x_min,
+        'x_max': x_max,
+        'data': [{'x': x, 'y': y} for x, y in zip(x_values, y_values)],
+    }
+
+    functions[id] = function_dict
 
     return redirect('/')
 
