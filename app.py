9688899de45401e76529140d0e1a2194fe9c1fc1
af0liveira <af0liveira@icloud.com>
2024-05-23 20:35:18 +0200

    Refactor app to use wtforms and a single endpoint
    The app will use wtforms, which has the advantage to facilitate form
    validation.
    
    No additional functionality is added.

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
 import numpy as np
 
-from flask import Flask, jsonify, render_template, redirect, request
+from flask import Flask, flash, render_template, redirect, request, url_for
+from flask_wtf import FlaskForm
+from wtforms import FloatField, SubmitField, StringField 
+from wtforms.validators import DataRequired, Regexp
+from markupsafe import Markup
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ah-ah-uh-uh'
+app.jinja_env.filters['zip'] = zip
 
-functions: list[dict]= []
+functions: list[dict] = []
+
+
+class AddFunctionForm(FlaskForm):
+    function = StringField(
+        Markup("f(x) ="),
+        validators=[
+            DataRequired(),
+            Regexp(r'^[\d\w\s\+\-\*\/\(\)\^]+$',
+                   message="Invalid function format"),
+        ],
+    )
+    xMin = FloatField(Markup("x<sub>min</sub> ="), validators=[DataRequired()])
+    xMax = FloatField(Markup("x<sub>max</sub> ="), validators=[DataRequired()])
+    submit = SubmitField("Add Function")
+
+
+class EditFunctionForm(FlaskForm):
+    function = StringField(
+        Markup("f(x) ="),
+        validators=[
+            DataRequired(),
+            Regexp(r'^[\d\w\s\+\-\*\/\(\)\^]+$',
+                   message="Invalid function format"),
+        ],
+    )
+    xMin = FloatField(Markup("x<sub>min</sub> ="), validators=[DataRequired()])
+    xMax = FloatField(Markup("x<sub>max</sub> ="), validators=[DataRequired()])
+    update = SubmitField('Update')
+    delete = SubmitField('Delete')
+
 
 def parse_function(f_expr: str, x_min: float, x_max: float) -> dict:
     x_values = np.linspace(x_min, x_max, num=101, endpoint=True)
@@ -21,36 +57,36 @@ def parse_function(f_expr: str, x_min: float, x_max: float) -> dict:
     return function
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', functions=functions)
+    add_form = AddFunctionForm()
+    edit_forms = [EditFunctionForm(prefix=str(i))
+                  for i, _ in enumerate(functions)]
 
-@app.route('/add_function/', methods=['POST'])
-def add_function():
-    function = request.form.get('addFunction')
-    x_min = float(request.form.get('setXMin'))
-    x_max = float(request.form.get('setXMax')) 
+    if add_form.validate_on_submit():
+        function = add_form.function.data
+        x_min = add_form.xMin.data
+        x_max = add_form.xMax.data
+        functions.append(parse_function(function, x_min, x_max))
+        flash("Funcionou, caboclo!")
+        print("PUTA QUE PARIU, DESGRACA!")
+        return redirect('/')
 
-    function_dict = parse_function(function, x_min, x_max)
-    functions.append(function_dict)
+    for i, form in enumerate(edit_forms):
+        if form.update.data and form.validate_on_submit():
+            function = form.function.data
+            x_min = form.xMin.data
+            x_max = form.xMax.data
+            functions[i] = parse_function(function, x_min, x_max)
+            flash("Funcionou, caboclo!")
+            return redirect('/')
+        elif form.delete.data:
+            del functions[i]
+            return redirect('/')
 
-    return redirect('/')
+    return render_template('index.html', functions=functions,
+                           add_form=add_form, edit_forms=edit_forms)
 
-@app.route('/delete_function/<int:id>', methods=['POST'])
-def delete_function(id: int):
-    del functions[id]
-    return redirect('/')
-
-@app.route('/update_function/<int:id>', methods=['POST'])
-def update_function(id: int):
-    function = request.form.get(f'function{id}')
-    x_min = float(request.form.get(f'setXMin{id}'))
-    x_max = float(request.form.get(f'setXMax{id}'))
-
-    function_dict = parse_function(function, x_min, x_max)
-    functions[id] = function_dict
-
-    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
