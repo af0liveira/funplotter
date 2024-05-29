18355ec173be9c04e0696137ac6f73ae4116789f
af0liveira <af0liveira@icloud.com>
2024-05-29 15:34:12 +0200

    Move JS code from index.html to a static .js file
    The script for generating the Chart.js graph is moved from
    `templates/index.html` to `static/chart.js` in order to separate
    responsibiliities a bit better and reduce the length of the HTML file.

---
+++ app/static/chart.js
@@ -0,0 +1,34 @@
+const ctx = document.getElementById('funPlots').getContext('2d');
+const lineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    datasets: dataSets, // NOTE: `dataSets` should be defined in `document`
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    elements: {
+      point: {
+        pointStyle: false
+      }
+    },
+    scales: {
+      x: {
+        type: 'linear',
+        position: 'bottom',
+        title: {
+          display: true,
+          text: 'x'
+        }
+      },
+      y: {
+        type: 'linear',
+        position: 'left',
+        title: {
+          display: true,
+          text: 'f(x)'
+        }
+      }
+    }
+  }
+});
